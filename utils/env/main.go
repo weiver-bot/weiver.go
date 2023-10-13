0229c24aab180df883e5e622f25103652d0840ac
y2hO0ol23 <y2h000123@naver.com>
2023-10-13 15:34:22 +0900

    env output

--- utils/env/main.go
+++ utils/env/main.go
@@ -1,15 +1,24 @@
 package env
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
 	err := godotenv.Load(".env")
-
 	if err != nil {
 		log.Fatalf("Error loading .env file\n%v", err)
 	}
+
+	fmt.Println("#### .env data ####")
+	fmt.Println("TOKEN       :", os.Getenv("Token"))
+	fmt.Println("MYSQL_URL   :", os.Getenv("MYSQL_URL"))
+	fmt.Println("PAGE_ROW    :", os.Getenv("PAGE_ROW"))
+	fmt.Println("ROLE_FORMAT :", os.Getenv("ROLE_FORMAT"))
+	fmt.Println("## .env data end ##")
+
 }
